dc27bd485425bf790891fa07aea57e1a93c44173
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-02-09 20:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student = {
+	name: "John"
+};
+
+Object.prototype.getKeys = function () {
+	return object.keys(this);
+};
+
+console.log(student.getKeys());
